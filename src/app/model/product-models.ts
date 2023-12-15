9a2a80c4f93d18fd156fd82479f6fb18af4d3bb7
Oli Hock <oliver.hock@gmail.com>
2023-12-15 11:24:12 +0100

    feat(products): update getProducts method and models
    - Update the return type of the getProducts method in the ProductsService to
      return an Observable of Pagination<Product[]> instead of Page<Product[]>.
    
    - Modify the URL in the getProducts method to include the sortBy and sortOrder
      parameters.
    
    - Update the import statement for Page in the products.service.ts file to
      import Pagination instead.
    
    - Add the Pagination and Product interfaces in the product-models.ts file.
    
    - Remove the Sort and Pageable interfaces from the product-models.ts file.
    
    - Update the Category and Condition interfaces in the product-models.ts file
      to remove the comments.
    
    - Remove the unused properties in the Category and Condition interfaces.
    
    - Reformat the code to adhere to the conventional commit message guidelines.

--- src/app/model/product-models.ts
+++ src/app/model/product-models.ts
@@ -1,48 +1,29 @@
-export interface Category {
-  id: string; // UUID
-  name: string;
-}
-
-export interface Condition {
-  id: string; // UUID
-  name: string;
+export interface Pagination<T> {
+  content: T
+  totalPages: number
+  totalElements: number
+  pageSize: number
+  pageNumber: number
+  sortBy: string
+  sortOrder: string
 }
 
 export interface Product {
-  id: string; // UUID
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-  category: Category;
-  condition: Condition;
+  id: string
+  name: string
+  description: string
+  price: number
+  image: string
+  category: Category
+  condition: Condition
 }
 
-export interface Page<T> {
-  totalElements: number;
-  totalPages: number;
-  size: number;
-  content: T;
-  number: number;
-  sort: Sort;
-  numberOfElements: number;
-  first: boolean;
-  last: boolean;
-  pageable: Pageable;
-  empty: boolean;
+export interface Category {
+  id: string
+  name: string
 }
 
-export interface Sort {
-  empty: boolean;
-  unsorted: boolean;
-  sorted: boolean;
-}
-
-export interface Pageable {
-  offset: number;
-  sort: Sort;
-  paged: boolean;
-  unpaged: boolean;
-  pageSize: number;
-  pageNumber: number;
+export interface Condition {
+  id: string
+  name: string
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs";
-import {Page, Product} from "../model/product-models";
+import {Pagination, Product} from "../model/product-models";
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,12 @@ export class ProductsService {
   constructor(private httpClient: HttpClient) {
   }
 
-  getProducts(sort: string = 'price', order: string = 'asc'): Observable<Page<Product[]>> {
+  getProducts(sort: string = 'price', order: string = 'asc'): Observable<Pagination<Product[]>> {
 
-    let url: string = `http://localhost:18080/api/search?sort=${sort},${order}`;
+    let url: string = `http://localhost:18080/api/search?search=&sortBy=${sort}&sortOrder=${order}`;
     console.log(`GET ${url}`);
 
-    let products: Observable<Page<Product[]>> = this.httpClient.get<Page<Product[]>>(url);
+    let products: Observable<Pagination<Product[]>> = this.httpClient.get<Pagination<Product[]>>(url);
     console.log(`products: ${products}`);
 
     return products;
